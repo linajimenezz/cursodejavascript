ee0f2ea4eaf7ed3895c70e1a5ab323fb14212f41
Lina Jimenez <lmjimenezz06@gmail.com>
2024-07-17 21:00:41 -0500

    Agregado trabalenguas por la P

--- CLASE0_BIENVENIDA/clase-java.js
+++ CLASE0_BIENVENIDA/clase-java.js
@@ -2,3 +2,10 @@ console.log("java")
 console.log("te amo")
 console.log("Lina Te amo")
 console.log("Cora y Hermi se aman mucho")
+
+```
+Pedro Pèrez Pereira
+pobre pintor portugues
+pinta preciosos paisajes 
+para poder partir para Parìs
+```
