b68bdbc80c8ab135f4b15460ed68ea8ad052a1f2
Lina Jimenez <lmjimenezz06@gmail.com>
2024-07-23 21:28:54 -0500

    clase de strings

---
+++ CLASE3_STRING/string.js
@@ -0,0 +1,17 @@
+//TIPO DE DATOS: STRING
+
+let string1 ="javascript es genial"
+let string2 = 'hola,mundo'
+let string3 = `${string1} feliz`
+let string4 = string1 +  string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
+
+let frase = "Javascript es extremadamente genial"
+console.log(frase.length)
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+
