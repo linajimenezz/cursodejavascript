eb002a64b9d8ad8f5999a325871f2936c80448db
Lina Jimenez <lmjimenezz06@gmail.com>
2024-07-24 20:45:17 -0500

    operaciones aritmeticas

---
+++ CLASE4_ OPERADORES ARITMETICOS/claseNumber.js
@@ -0,0 +1,47 @@
+// 1. tipo entero y decimal 
+const entero = 29
+const decimal = 3.14
+console.log (typeof entero, typeof decimal)
+
+// 2. notacion cientifica
+const cientifico = 5e3
+
+// 3. infinitos y NaN
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+// operaciones aritmeticas
+
+// 1. suma, resta, multiplicacion y division
+const suma = 3 + 4
+const resta = 4 -4
+const multiplicacion = 4 * 7
+const division = 16 / 2
+
+// 2. modulo y exponenciacion
+const modulo = 15 % 8
+const exponenciacion = 2 ** 3
+
+// presicion 
+const resultado = 0.1 + 0.2 
+console.log (resultado)
+console.log (resultado. toFixed(1))
+console.log (resultado === 0.3)
+
+// operaciones avanzadas 
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(6)
+const aleatorio = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
+
+
+
+
+
+
+
+
+
+
