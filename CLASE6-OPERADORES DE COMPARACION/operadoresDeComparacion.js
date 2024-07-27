6996b24ddd7adf6ed2209ba3fd727f69089fe1e0
Lina Jimenez <lmjimenezz06@gmail.com>
2024-07-26 22:00:14 -0500

    operadores de comparacion y logicos

---
+++ CLASE6-OPERADORES DE COMPARACION/operadoresDeComparacion.js
@@ -0,0 +1,23 @@
+/*
+
+operadores de comparacion
+
+*/
+const a = 10
+const b = 20
+const c = "10"
+
+
+// a == b
+// a=== b
+// a != c
+// !==
+// <
+// >
+// >=
+// >=
+
+
+console.log(a == b)
+console.log(a === c)
+console.log(a == c)
---
+++ CLASE6-OPERADORES DE COMPARACION/operadoresLogicos.js
@@ -0,0 +1,16 @@
+/*
+
+operadores logicos
+
+*/
+
+
+// &&
+// ||
+// !
+
+const a = 10;
+const b = 20;
+const c = "10"
+
+
